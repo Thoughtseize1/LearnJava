e64889f8718f1e2d1bea4bce0bc1da2dd2610a34
Nikita Sherstianykh <n.sherstianykh@gmail.com>
2023-04-01 18:26:26 +0200

    FirstFile

---
+++ src/First.java
@@ -0,0 +1,14 @@
+public class First {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        String myString = "This is a string!";
+        System.out.println("myString is equal to: " + myString);
+        String addingString = "_PLUS_";
+        myString = myString + "CHUVAK";
+        System.out.println(myString);
+        myString = myString + addingString;
+        System.out.println(myString);
+
+    }
+}
